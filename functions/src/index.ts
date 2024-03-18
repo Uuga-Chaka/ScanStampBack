5d0f26db12c02376ab16c25e2563dce80e36ce34
Juan Gallego <jpgarias09@gmail.com>
2024-03-17 21:27:14 -0500

    handle user validation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,18 @@
 import { onSchedule } from 'firebase-functions/v2/scheduler'
 import * as logger from 'firebase-functions/logger'
+import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
 admin.initializeApp()
 
+export const handleUserValidation = functions.auth.user().onCreate(async (user) => {
+  logger.info('Adding user to DB')
+  await admin.database().ref(`/api/admin/${user.uid}`).set({ validated: false })
+  logger.info('Finished adding user to DB')
+})
+
 export const handleDailyCleanup = onSchedule('every day 00:00', async () => {
-  logger.info('stated clean up')
-  try {
-    const ref = await admin.database().ref('/api')
-    await ref.remove()
-    logger.info('Scheduled daily cleanup completed successfully.')
-  } catch (err) {
-    logger.error(`Error occurred during scheduled cleanup: ${err}`)
-  }
+  logger.info('Started clean up')
+  await admin.database().ref('/api').remove()
+  logger.info('Scheduled daily cleanup completed successfully.')
 })
