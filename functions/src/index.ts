fc516b5557207510a3630d950b7361f34db0fe3c
Juan Gallego <jpgarias09@gmail.com>
2024-03-20 18:49:01 -0500

    feat: :sparkles: Event listener for updated on admins
    When an admin is updated its validated property on the database it will also be updated its custom claims to the updated value
    
    Custom claims updated aligned with the db

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import { onSchedule } from 'firebase-functions/v2/scheduler'
+import { onValueUpdated } from 'firebase-functions/v2/database'
 import * as logger from 'firebase-functions/logger'
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
@@ -6,13 +7,24 @@ import * as admin from 'firebase-admin'
 admin.initializeApp()
 
 export const handleUserValidation = functions.auth.user().onCreate(async (user) => {
-  logger.info('Adding user to DB')
+  logger.info('Setting users claims')
   await admin.auth().setCustomUserClaims(user.uid, { validated: false })
-  await admin
-    .database()
-    .ref(`/api/admin/${user.uid}`)
-    .set({ validated: false, email: user.email, uid: user.uid })
+  logger.info('Finish setting user claims')
+  logger.info('Adding user to DB')
+  await admin.database().ref(`/api/admin/${user.uid}`).set({ email: user.email, uid: user.uid })
   logger.info('Finished adding user to DB')
+})
+
+export const setUserRoles = onValueUpdated('/api/admin/{uid}', (event) => {
+  if (!event.data.after.exists()) return
+
+  const { validated } = event.data.after.val()
+
+  if (!validated) return
+
+  const uid = event.params.uid
+  logger.info('Setting up user permissions')
+  return admin.auth().setCustomUserClaims(uid, { validated })
 })
 
 export const handleDailyCleanup = onSchedule('every day 00:00', async () => {
